7e7382754e5381553335d9698ccd42ab14c5b9e1
Sam .M <you@example.com>
2023-09-01 08:21:58 +0300

    done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -5,8 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Student(Base):
+    __tablename__ = "students"
     pass
 
+
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
